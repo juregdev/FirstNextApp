65fd376d68fcaf213e794fe7a9301ad532dd3857
Filipe Barros <filipe.araujo9@outlook.com>
2022-06-17 15:02:14 -0300

    feat include: faunadb inserts and verification

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+
+import { query as q } from 'faunadb'
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -13,5 +16,37 @@ export default NextAuth({
       }
     }),
   ],
+  callbacks: {
+    async signIn(data) {
+      const { user } = data
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('data_by_email'),
+                  q.Casefold(data.user.email!)
+                )
+              )
+            ), q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(q.Match(
+              q.Index('data_by_email'), q.Casefold(data.user.email!)
+            ))
+          )
+        )
+        return true
+      } catch (err) {
+        console.log(err)
+        return false
+      }
+    }
+
+  }
 }
 )
