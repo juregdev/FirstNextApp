bf978cd91d5fdaa618ed7926de3993c7bd160f62
Filipe Barros <filipe.araujo9@outlook.com>
2022-06-20 18:09:30 -0300

    feat: added method for don´t duplicate customer in stripe

--- src/pages/api/subcription.ts
+++ src/pages/api/subcription.ts
@@ -1,17 +1,54 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { query as q } from 'faunadb'
 import { getSession } from "next-auth/react";
 import { stripe } from "../../services/stripe";
+import { fauna } from "../../services/fauna";
+
+type User = {
+  ref: { id: string, },
+  data: { stripeCostumerId: string }
+
+}
 
 /* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const session = await getSession({ req });
-    const stripeCreateCostumer = await stripe.customers.create({
-      email: session?.user?.email!
-    })
+
+
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('data_by_email'),
+          q.Casefold(session?.user?.email!)
+        )
+      )
+    )
+
+    let customerId = user.data.stripeCostumerId
+
+    if (!customerId) {
+      const stripeCreateCostumer = await stripe.customers.create({
+        email: session?.user?.email!
+      })
+
+      await fauna.query(
+        q.Update(
+          q.Ref(
+            q.Collection('users'), user.ref.id),
+          {
+            data: { stripeCostumerId: stripeCreateCostumer.id }
+          }
+
+        )
+      )
+
+      customerId = stripeCreateCostumer.id
+    }
+
 
     const StripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCreateCostumer.id,
+      customer: customerId,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
